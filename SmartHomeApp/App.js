6ba7cb24689abfefa1a4d6681788d9cd7648dc64
Doan Tran Huu Phuoc <phuoc.doan0412@hcmut.edu.vn>
2021-05-30 20:11:29 +0700

    Phuoc test mqtt lan 1

--- SmartHomeApp/App.js
+++ SmartHomeApp/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import NavBar from "./source/components/navBar";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
+import MQTTConnection from "./source/mqtt/MQTTConnection";
+import { Buffer } from 'buffer';
+import { useEffect } from "react";
 
 export default function App() {
   return (
--- SmartHomeApp/source/notification-screen/notificationScreen.jsx
+++ SmartHomeApp/source/notification-screen/notificationScreen.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
-import {Text, View, Image, TouchableOpacity, Dimensions, Switch} from "react-native";
+import React, { useEffect } from "react";
+import { Button } from "react-native";
 import { createStackNavigator } from '@react-navigation/stack';
-import Images from "../config/images";
-import Data from "../database/data";
+import MQTTConnection from "../mqtt/MQTTConnection";
+import { Buffer } from 'buffer';
 
+global.Buffer = Buffer;
 const NotificationStack = createStackNavigator();
+
 
 function NotificationStackScreen() {
     return (
@@ -24,10 +26,43 @@ function NotificationStackScreen() {
       </NotificationStack.Navigator>
     );
   }
+
 const NotificationScreen = () => {
+  useEffect(() => {
+    this.mqttConnect = new MQTTConnection()
+    this.mqttConnect.onMQTTConnect = this.onMQTTConnect
+    this.mqttConnect.onMQTTLost = this.onMQTTLost
+    this.mqttConnect.onMQTTMessageArrived = this.onMQTTMessageArrived
+    this.mqttConnect.onMQTTMessageDelivered = this.onMQTTMessageDelivered
+    
+    this.mqttConnect.connect('broker.mqttdashboard.com', 8000)
+
+    onMQTTConnect = () => {
+      console.log('App onMQTT Connected!')
+      this.mqttConnect.subscribeChannel('SIHS')
+    }
+
+    onMQTTLost = () => {
+      console.log('App onMQTT Lost')
+    }
+
+    onMQTTMessageArrived = (message) => {
+      console.log('App onMQTTMessageArrive: ' + message.payloadString)
+    }
+
+    onMQTTMessageDelivered = (message) => {
+      console.log('App onMQTTMessageDelivered: ' + message.payloadString);
+    }
+
+    return() => {
+      this.mqttConnect.close()
+    }
+
+  }, [])
+  
   return (
     <React.Fragment>
-      <Text>Notification Screen Code Here</Text>
+      <Button title = "Press" onPress = {() => this.mqttConnect.send('SIHS', "This is a message!")}/>
     </React.Fragment>
   );
 };
